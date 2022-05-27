28b2b2107b469d693dd421c892f1e71016292cbc
maiquelb <maiquelb@gmail.com>
2022-05-27 15:44:14 -0300

    update perception actio  example to embedded-mas 0.033

--- examples/perception_action/src/main/java/MyRosMaster.java
+++ examples/perception_action/src/main/java/MyRosMaster.java
@@ -7,14 +7,14 @@ import java.util.Collection;
 import embedded.mas.bridges.jacamo.JSONWatcherDevice;
 import embedded.mas.bridges.jacamo.IPhysicalInterface;
 import embedded.mas.bridges.jacamo.DefaultDevice;
-import embedded.mas.bridges.ros.RosDevice;
+import embedded.mas.bridges.jacamo.LiteralDevice;
 import embedded.mas.bridges.ros.DefaultRos4EmbeddedMas;
 import jason.asSyntax.Atom;
 import jason.asSyntax.Literal;
 
 
 
-public class MyRosMaster extends RosDevice {
+public class MyRosMaster extends LiteralDevice {
 
 	public MyRosMaster(Atom id, DefaultRos4EmbeddedMas microcontroller) {
 		super(id, microcontroller);
