9e583bc47333b4523568c7b9d8f709509bda0998
maiquelb <maiquelb@gmail.com>
2022-05-30 14:52:37 -0300

    move examples to version 0.034 of embedded-mas

--- examples/services/src/main/java/DemoEmbeddedAgentROS.java
+++ examples/services/src/main/java/DemoEmbeddedAgentROS.java
@@ -21,11 +21,13 @@ public class DemoEmbeddedAgentROS extends EmbeddedAgent {
 	protected void setupSensors() {
 	
 		
-               /*addTopic("value1", "std_msgs/Int32");
-               addTopic("value2", "std_msgs/Int32");
-               addTopic("current_time", "std_msgs/String");*/
+               /*
+               //Testing topic-belief conversion. Uncomment and verify Jason mind inspector to check it out.
                
-               //addTopic("turtle1/pose", "turtlesim/Pose");
+               addTopic("turtle1/cmd_vel", "geometry_msgs/Twist");
+               addTopic("turtle1/color_sensor", "turtlesim/Color");
+               addTopic("turtle1/pose", "turtlesim/Pose");
+               */
                
                /* roscore1 is a connection with a ros master. Instantiate new DefaultRos4EmbeddedMas connect the agent with more ros masters*/
 		DefaultRos4EmbeddedMas roscore1 = new DefaultRos4EmbeddedMas("ws://localhost:9090",nodes, topics);		
