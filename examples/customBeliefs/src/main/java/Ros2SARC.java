6a43139456fdc58d64ebb8924b77a2514cb905e9
maiquelb <maiquelb@gmail.com>
2022-06-27 23:08:31 -0300

    update customBeliefs example

--- examples/customBeliefs/src/main/java/Ros2SARC.java
+++ examples/customBeliefs/src/main/java/Ros2SARC.java
@@ -15,18 +15,30 @@ public class Ros2SARC extends DefaultRos4EmbeddedMas{
    }
    
    
+   /**
+    * Customizes beliefs when should not follow the default format translated from rostopics.
+    * By default, returns null. In this case, the belief follows the default translation from the rostopic.
+    * To return values other than null, override this method in subclasses. In this case, the method is supposed to build and return a literal representing
+    * the belief corresponding to the topic. 
+    * 
+    * @param topicName: name of the topic to be converted to belief.
+    * @param data: JSON containing the topic values. This JSON is produced by the rosbridge, which is an interface ROS-Java. To inspect this JSON, print the value of this parameter. 
+    * @return null to follow the default conversion from topic value to belief; a literal representing the corresponding belief otherwise.
+   */
    @Override
-   public Literal customizeBelief(String topicName, JsonNode data) {
-      if(topicName.equals("turtle1/pose")){
-         // Belief creation: the first argument is the functor (i.e. the "name" of the belief) and the following are the belief argumentss
+   protected Literal customizeBelief(String topicName, JsonNode data) {
+      if(topicName.equals("turtle1/pose")){ 
+         // System.out.println(data.toString()); //uncomment to inspect the JSON to be handled
+         
+         // The parameter data looks like {"x":0.1,"y":0.2,"theta":0.3,"linear_velocity":0.0,"angular_velocity":0.0}}. It is converted to position(0.1,0.2).
          // To disable this customization, comment the line below
          return createLiteral("position",createAtom(data.get("x").toString()),createAtom(data.get("y").toString()));
       }else
       
-      /***** Tópicos relacionados à câmera - alterar para transformar em crença ******/
       
+      /***** Tópicos relacionados à câmera - alterar para transformar em crença ******/      
       if(topicName.equals("uavX/rgbd_down/aligned_depth_to_color/image_raw")){
-         System.out.println(data.toString()); //print the json to be handled to extract the belief values
+         System.out.println(topicName + ": " + data.toString()); //print the json to be handled to extract the belief values
          return null;
       }else
       if(topicName.equals("uavX/rgbd_down/aligned_depth_to_color/image_raw/compressed")){
